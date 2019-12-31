3f6e037f7af12d30babe71dd23ad1acd6f042a90
tahir-techverx <57141102+tahir-techverx@users.noreply.github.com>
2019-12-31 13:19:10 +0500

    update

--- 4500BioMetricApp/ConnectionString.cs
+++ 4500BioMetricApp/ConnectionString.cs
@@ -8,6 +8,6 @@ namespace _4500BioMetricApp
 {
    public static class ConnectionString
     {
-        public static String con_string = @"Server=" + MainWindow.Server + "; port=" + MainWindow.Port + "; Database=" + MainWindow.Database + "; Uid=" + MainWindow.Uid + "; Pwd=7120020@123; default command timeout=99999;CHARSET=utf8";
+        public static String con_string = @"Server=" + MainWindow.Server + "; port=" + MainWindow.Port + "; Database=" + MainWindow.Database + "; Uid=" + MainWindow.Uid + "; Pwd=7120020@123; default command timeout=9999;CHARSET=utf8";
     }
 }
